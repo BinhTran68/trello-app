47fa987ba9cd2b2baa3d0965bd18cd3b0602ec9e
BinhTran68 <binhtd2268@gmail.com>
2024-01-22 18:27:44 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { pink } from '@mui/material/colors'
+import HomeIcon from '@mui/icons-material/Home'
+import Button from '@mui/material/Button'
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+  useColorScheme
+} from '@mui/material/styles'
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function ModeSelect() {
+  // const [age, setAge] = React.useState('');
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    // setAge(event.target.value)
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="demo-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }} >
+                Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+        <MenuItem value="system">System</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+
+      <ModeSelect/>
+      <hr/>
+      <ModeToggle/>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+                Trần Bình
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button color="error" variant="contained">Hello world</Button>
+      <Button variant="text">Hello world</Button>
+      <Button variant="outlined">Hello world</Button>
+
+
+      <HomeIcon/>
+      <HomeIcon color="primary"/>
+      <HomeIcon color="secondary"/>
+      <HomeIcon color="success"/>
+      <HomeIcon color="action"/>
+      <HomeIcon color="disabled"/>
+      <HomeIcon sx={{ color: pink[500] }}/>
+
+
     </>
   )
 }
